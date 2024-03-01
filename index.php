e018e7ffca60bbe31e9c6db7a079499878b8bc05
“Essex <eigyan@gmail.com>
2024-03-01 02:51:13 -0500

    add copy paste textarea containing conversion

--- index.php
+++ index.php
@@ -145,6 +145,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['markdown'])) {
     </form>
 
     <hr />
+    <textarea id="foo" name="markdown" rows="20"
+        cols="50"><?php echo isset($_POST['markdown']) ? htmlspecialchars($html) : ''; ?></textarea><br>
+    <hr />
     <br>
     <div>
         <?php echo $html ?>
